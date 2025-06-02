926e89cf238547af5592d3101a18eab020340e26
hassaammgl <h455a4mmehtab@gmail.com>
2025-06-02 22:35:17 +0500

    fix: enhance database connection logging and ensure successful connection message

--- api/index.js
+++ api/index.js
@@ -3,8 +3,10 @@ import app from './src/app.js';
 import { CONSTANTS } from './src/utils/constants.js';
 
 console.time("connecting db")
-await connectDB();
-console.timeEnd("connecting db")
+await connectDB().then(() => {
+    console.log("connected !");
+    console.timeEnd("connecting db")
+});
 
 const PORT = CONSTANTS.PORT || 3000;
 app.listen(PORT, () => {
