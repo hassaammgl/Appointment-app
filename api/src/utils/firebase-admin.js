14e5bea9756b871257515667dae5f58b001b4a8b
hassaammgl <h455a4mmehtab@gmail.com>
2025-07-09 03:31:02 +0500

    feat: implement Firebase messaging functionality; enhance notification sending logic

--- api/src/utils/firebase-admin.js
+++ api/src/utils/firebase-admin.js
@@ -1,7 +1,28 @@
-import admin from "firebase-admin"
-import serviceAccount from "../config/sapps.json"
+import admin from 'firebase-admin';
 
+const serviceAccount = require('../service-account.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
+
+export const messaging = admin.messaging();
+
+
+export const sendNotification = async (userTokens, title, body) => {
+    try {
+        const response = await messaging.sendMulticast({
+            tokens: userTokens,
+            notification: { title, body },
+            webpush: {
+                headers: {
+                    Urgency: 'high'  // For background notifications
+                }
+            }
+        });
+
+        console.log(`${response.successCount} messages sent`);
+    } catch (error) {
+        console.error('Error sending message:', error);
+    }
+}
