037ac57c6b38f7b9536a5e7c25d03cfc2ba4002b
hassaammgl <h455a4mmehtab@gmail.com>
2025-10-08 04:54:48 +0500

    refactor: replace hardcoded OneSignal appId with environment variable

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -18,8 +18,8 @@ export function useOneSignal() {
     window.OneSignalDeferred = window.OneSignalDeferred || [];
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     window.OneSignalDeferred.push(async function (OneSignal: any) {
-     const app = await OneSignal.init({
-        appId: "891f37e4-5f76-404b-8751-d057d3545742",
+      const app = await OneSignal.init({
+        appId: import.meta.env.VITE_ONESIGNAL_APP_ID!,
         safari_web_id:
           "web.onesignal.auto.3b8b9214-66ac-44d1-a7fb-a9dc856242cb",
         notifyButton: {
@@ -28,7 +28,6 @@ export function useOneSignal() {
         allowLocalhostAsSecureOrigin: true,
       });
       console.log(app);
-      
     });
   }, []);
 }
